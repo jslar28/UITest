f5e62bf29b641d457c15539d20c151aaeacea2fa
Jakob Larsen <jslar28@gmail.com>
2020-07-15 16:43:23 +0200

    Added button to show text

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public GameObject text;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            bool state = text.gameObject.activeInHierarchy;
+            text.gameObject.SetActive(!state);
+        }
+    }
+}
